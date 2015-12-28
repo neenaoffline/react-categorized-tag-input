85909f32a03ae7ea76bf937df14ff0f47de38f5f
Ravi Chandra Padmala <neena@nilenso.com>
2015-12-28 13:35:44 +0530

    Work in progress: asynchronous category autocomplete

--- index.js
+++ index.js
@@ -29,15 +29,50 @@ const categories = [
   }
 ];
 
+const categoryIter = function (i) {
+  return [
+    {
+      id: "animals",
+      title: 'Animals',
+      type: 'animal',
+      items: [`${i}Dog`, `${i}Cat`, `${i}Bird`, `Dolphin${i}`, `Apes${i}`]
+    },
+    {
+      id: 'something',
+      title: 'Something cool',
+      items: ['Something cool'],
+      single: true
+    },
+    {
+      id: 'food',
+      title: 'food',
+      type: 'food',
+      items: ['Apple', 'Banana', 'Grapes', 'Pear']
+    },
+    {
+      id: 'professions',
+      title: 'Professions',
+      type: 'profession',
+      items: ['Waiter', 'Writer', 'Hairdresser', 'Policeman']
+    }
+  ];
+};
+
 function transformTag(category, item) {
   return `${category}/${item}`;
 }
+
+let i = 0;
 
 const props = {
   addNew: true,
   categories,
   transformTag,
   value: ['initial'],
+  getCategories: (value) => {
+    console.log('suggestions', {suggestionsFor : value});
+    return new Promise((resolve) => { i = i + 1; resolve(categoryIter(i)) });
+  },
   onChange(tags) {
     console.log('Changed', tags);
   },
